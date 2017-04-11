47c5869abb466f5a228da56e8a355e07c3edb6d2
yubinhong <yubinhong@miguantech.com>
2017-04-11 09:53:12 +0800

    修改软件汇总，判断是否为一列，如果是，进行分割

--- backend/main.py
+++ backend/main.py
@@ -242,8 +242,8 @@ def ruanjian(dir):
             countList = sh.col_values(start_rowx=1, colx=1)
         elif sh.ncols == 1:
             resultList=sh.col_values(start_rowx=1,colx=0)
-            timeList=[x.spilit('\t')[0] for x in resultList]
-            countList=[x.spilit('\t')[1] for x in resultList]
+            timeList=[x.split('\t')[0] for x in resultList]
+            countList=[x.split('\t')[1] for x in resultList]
         try:
             timeList = [time.strptime(x.strip(), "%Y%m%d") for x in timeList]
 
